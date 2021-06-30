2ebda340bfb0618019ea5a8493bbf8865534091e
Shruti Verma <shrutiverma3105@gmail.com>
2021-06-30 17:16:55 +0530

    event binding: new tab on clicking logo

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import "./App.css";
 import logo from "./img/BridgeLabzLogo.svg";
 
 class App extends React.Component {
+  url = "http://www.bridgelabz.com/";
   constructor() {
     super();
     this.state = {
       title: "Hello from BridgeLabz",
     };
   }
+
+  //onClick event
+  onClick = ($event) => {
+    console.log("save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  };
   render() {
     return (
       <div>
         <h1>{this.state.title}</h1>
         <img
           src={logo}
+          onClick={this.onClick}
           alt="The Bridgelabz logo: a Bridge to Employment through lab works"
         />
       </div>
