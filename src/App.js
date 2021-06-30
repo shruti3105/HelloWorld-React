f4735c120c94c720d51c69c507573e3613aeade8
Shruti Verma <shrutiverma3105@gmail.com>
2021-06-30 17:32:37 +0530

    user input

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      title: "Hello from BridgeLabz",
+      userName: "",
     };
   }
 
@@ -16,16 +16,26 @@ class App extends React.Component {
     console.log("save button is clicked!", $event);
     window.open(this.url, "_blank");
   };
+
+  onNameChange = ($event) => {
+    console.log("value is ", event.target.value);
+    this.setState({ userName: event.target.value });
+  };
   render() {
     return (
-      <div>
-        <h1>{this.state.title}</h1>
-        <img
-          src={logo}
-          onClick={this.onClick}
-          alt="The Bridgelabz logo: a Bridge to Employment through lab works"
-        />
-      </div>
+      <>
+        <div>
+          <h1>Hello {this.state.userName} from Bridgelabz</h1>
+          <img
+            src={logo}
+            onClick={this.onClick}
+            alt="The Bridgelabz logo: a Bridge to Employment through lab works"
+          />
+        </div>
+        <div>
+          <input onChange={this.onNameChange} />
+        </div>
+      </>
     );
   }
 }
